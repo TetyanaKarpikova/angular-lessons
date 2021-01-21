4e962a2fae739e3f43bdd5516c693dd3256bd4ff
Tetyana <tetyanajkn@gmail.com>
2021-01-21 18:56:32 +0200

    users with address

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { UsersComponent } from './users/users.component';
+import { UserComponent } from './user/user.component';
+import { AddressComponent } from './address/address.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    UsersComponent,
+    UserComponent,
+    AddressComponent
   ],
   imports: [
     BrowserModule
